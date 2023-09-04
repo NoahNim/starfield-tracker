6d545c3f24c0be2cecd513bd854098908b05a3eb
Noah Medoff <noahmedoff@gmail.com>
2023-09-04 14:46:41 -0700

    had to delete all typescript stuff and restart as sequelize-cli etc really doesn't like typescript

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,23 @@
-const config = require('./index');
-
-const db = config.db;
-const username = db.username;
-const password = db.password;
-const database = db.database;
-const host = db.host;
-
-module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
 }
